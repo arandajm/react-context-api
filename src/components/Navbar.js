634b4c7386f44287365323447c9825e054091ff6
Jesus Aranda <jesusmaria.aranda@mercadolibre.com>
2021-07-23 18:56:34 -0300

    [FTR] - Context: - Added theme icon into context state. - Updated context state from navbar component using a shared context function.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,26 @@ export default class Navbar extends React.Component {
     return (
       <ThemeContext.Consumer>
         {(context) => {
-          const { isDarkTheme, lightTheme, darkTheme } = context;
+          const { isDarkTheme, lightTheme, darkTheme, handleChangeTheme } =
+            context;
           // Decide which theme is active
-          const { text, background } = isDarkTheme ? darkTheme : lightTheme;
+          const { text, background, themeIcon } = isDarkTheme
+            ? darkTheme
+            : lightTheme;
           return (
             <nav style={{ background, color: text, height: "120px" }}>
-              <h2 style={{ textAlign: "center" }}>TODO List</h2>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  padding: "10px",
+                }}
+              >
+                <h2 style={{ margin: "0 10px 0" }}>TODO List</h2>
+                <button className="ui icon button" onClick={handleChangeTheme}>
+                  <i className={`${themeIcon} icon`}></i>
+                </button>
+              </div>
               <div className="ui three buttons">
                 <button className="ui button">Overview</button>
                 <button className="ui button">Contact</button>
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -8,16 +8,23 @@ export default class ThemeContextProvider extends React.Component {
     lightTheme: {
       text: "#222",
       background: "#d8ddf1",
+      themeIcon: "sun",
     },
     darkTheme: {
       text: "#fff",
       background: "#5c5c5c",
+      themeIcon: "moon",
     },
   };
 
+  handleOnChangeTheme = () =>
+    this.setState({ isDarkTheme: !this.state.isDarkTheme });
+
   render() {
     return (
-      <ThemeContext.Provider value={{ ...this.state }}>
+      <ThemeContext.Provider
+        value={{ ...this.state, handleChangeTheme: this.handleOnChangeTheme }}
+      >
         {this.props.children}
       </ThemeContext.Provider>
     );
