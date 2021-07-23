d3b4f5e3f8d38333e721419a8f3212339748b27c
Jesus Aranda <jesusmaria.aranda@mercadolibre.com>
2021-07-23 18:03:59 -0300

    [FTR]: Context - Consumed context state using consumer into Navbar and TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,26 +2,29 @@ import React from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 export default class TodoList extends React.Component {
-  // Access to data context
-  static contextType = ThemeContext;
-
   render() {
-    const { isDarkTheme, lightTheme, darkTheme } = this.context;
-    // Decide which theme is active
-    const { text, background } = isDarkTheme ? darkTheme : lightTheme;
     return (
-      <div
-        style={{
-          background,
-          color: text,
-          height: "140px",
-          textAlign: "center",
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isDarkTheme, lightTheme, darkTheme } = context;
+          // Decide which theme is active
+          const { text, background } = isDarkTheme ? darkTheme : lightTheme;
+          return (
+            <div
+              style={{
+                background,
+                color: text,
+                height: "140px",
+                textAlign: "center",
+              }}
+            >
+              <p className="item">Go for a walk</p>
+              <p className="item">Go for shopping</p>
+              <p className="item">Other todo item!</p>
+            </div>
+          );
         }}
-      >
-        <p className="item">Go for a walk</p>
-        <p className="item">Go for shopping</p>
-        <p className="item">Other todo item!</p>
-      </div>
+      </ThemeContext.Consumer>
     );
   }
 }
